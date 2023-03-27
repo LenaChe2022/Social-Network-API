20d31a76f3d63b83b3e876d90379ed213c4a3480
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-03-26 19:22:06 -0700

    created all thoughts controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,17 +23,13 @@ module.exports = {
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
           : res.json(user)
-            // {
-            //   user,
-            //   grade: await grade(req.params.userId),
-            //})
       )
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
   },
-  // create a new user
+  //Create a new user
   createUser(req, res) {
     User.create(req.body)
       .then((user) => res.json(user))
@@ -58,30 +54,16 @@ module.exports = {
       });
    },
 
-  // Delete a user and remove a user's associated thoughts.
+  //Delete a user and remove a user's associated thoughts.
   deleteUser(req, res) {
-    User.findOneAndRemove({ _id: req.params.userId})
-  //TODO: ???? remove a user's associated thoughts:  
+    User.findOneAndDelete({ _id: req.params.userId})
+  //OK remove a user's associated thoughts:  
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No such user exists' })
-          : res.json({ message: `User deleted forever!` })
-          //: Thought.deleteMany({ _id: { $in: user.thoughts} })
-          
-          // .findOneAndUpdate(
-          //   { _id: req.params.userId },
-          //   { $pull: { thought: { thoughtId: req.params.thoughtId } } },
-          //   { runValidators: true, new: true }
-          //   )
+          : Thought.deleteMany({ _id: {$in: user.thoughts}})
       )
-   //TODO:  remove a user's associated thoughts.  
-      // .then((course) =>
-      //   !course
-      //     ? res.status(404).json({
-      //         message: 'Student deleted, but no courses found',
-      //       })
-      //     : res.json({ message: 'Student successfully deleted' })
-      // )
+      .then(() => res.json({ message: `User with all their thoughts deleted forever!` }))
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
@@ -129,40 +111,4 @@ module.exports = {
 
   },
 
-
-
-  // Add an assignment to a student
-  // addAssignment(req, res) {
-  //   console.log('You are adding an assignment');
-  //   console.log(req.body);
-  //   Student.findOneAndUpdate(
-  //     { _id: req.params.studentId },
-  //     { $addToSet: { assignments: req.body } },
-  //     { runValidators: true, new: true }
-  //   )
-  //     .then((student) =>
-  //       !student
-  //         ? res
-  //             .status(404)
-  //             .json({ message: 'No student found with that ID :(' })
-  //         : res.json(student)
-  //     )
-  //     .catch((err) => res.status(500).json(err));
-  // },
-  // // Remove assignment from a student
-  // removeAssignment(req, res) {
-  //   Student.findOneAndUpdate(
-  //     { _id: req.params.studentId },
-  //     { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
-  //     { runValidators: true, new: true }
-  //   )
-  //     .then((student) =>
-  //       !student
-  //         ? res
-  //             .status(404)
-  //             .json({ message: 'No student found with that ID :(' })
-  //         : res.json(student)
-  //     )
-  //     .catch((err) => res.status(500).json(err));
-  // },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,7 +5,7 @@ const {
   getSingleThought,
   updateThought,
   deleteThought,
-} = require('../../controllers/thoughtController.js');
+} = require('../../controllers/thoughtController');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
@@ -18,16 +18,15 @@ router.route('/').get(getThoughts).post(createThought);
 
 
 // /api/thoughts/:thoughtId
-router
-  .route('/:thoughtId')
-  .get(getSingleThought)
-  .put(updateThought)
-  .delete(deleteThought);
+router.route('/:thoughtId')
+   .get(getSingleThought)
+   .put(updateThought)
+   .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(createReaction)
+//router.route('/:thoughtId/reactions').post(createReaction)
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+//router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
